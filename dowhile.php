9b91c6b7a91beb3da2459ab3eb98d146339eff27
leonardobmessias <71788533+leonardobmessias@users.noreply.github.com>
2021-02-20 10:56:00 -0300

    dowhile

---
+++ dowhile.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div>
+    <?php
+    $c= 1;
+    do{
+        echo $c;
+        echo "<br>";
+        $c++;
+    }while($c <= 10);
+
+
+    ?>
+</div>
+
+</body>
+</html>

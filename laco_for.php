871ad4cd7f12fc98459025f9c55b6acf76f05ea8
leonardobmessias <71788533+leonardobmessias@users.noreply.github.com>
2021-02-21 10:09:18 -0300

    faco_for

---
+++ laco_for.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div>
+    <?php
+   for($c=1;$c<10;$c++){
+     echo "contando $c<br>";
+   }
+
+
+    ?>
+</div>
+
+</body>
+</html>

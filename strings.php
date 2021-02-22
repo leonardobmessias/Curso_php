25847d503c600e4718be4fc38004fa3f2c1e0fb0
leonardobmessias <71788533+leonardobmessias@users.noreply.github.com>
2021-02-22 15:43:47 -0300

    atualização

---
+++ strings.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div><pre>
+    <?php
+    $x =[[2,1,3,4],[2,5,4,7]];
+    print_r($x);
+    $nome = "Leonardo";
+    $tamanho = strlen($nome);
+    echo '<br>';
+    echo strlen($nome);
+    echo '<br>';
+
+    ?>
+    </pre>
+</div>
+
+</body>
+</html>
